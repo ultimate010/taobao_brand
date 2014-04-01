00dc8ad204510314e777829f2c15badbf558d12b
ultimate010 <ultimate010@gmail.com>
2014-04-01 15:33:15 +0800

    Add taobao_cat.py

---
+++ taobao_cat.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+import time
+import datetime
+from fetcher import fetcher
+import sys
+import re
+
+threadsNum = 16
+
+
+def main():
+    if len(sys.argv) != 4:
+        print "Usage: python %s urlFile outfile skipNum 1> outFile 2>logFile" % sys.argv[0]
+        exit(1)
+    count = 0
+    pattern = re.compile("rootCatId=([0-9]+)", re.M)
+    f = fetcher(threads=threadsNum , failfiles = "./fetcher.log")
+    fileout = open(sys.argv[2],'w')
+    with open(sys.argv[1],'r') as fi:
+        for line in fi:
+            line = line.strip('\r\n')
+            if(len(line) == 0):
+                continue
+            count += 1
+            if(count < int(sys.argv[3])):
+                continue
+            line = line.decode('utf-8','ignore')
+            arr = line.split('\t')
+            if(count % 10 == 0):
+                sys.stderr.write("%s:Doing %d %s\n" % (datetime.datetime.now(),count,arr[0].encode('utf-8')))
+            try:
+                mystr = arr[0]
+                for i in range(len(arr) - 1):
+                    #print arr[i+1]
+                    #raw_input()
+                    f.push(arr[i+1])
+                while f.taskleft():
+                    url , content = f.pop()
+                    ans = pattern.findall(content)
+#                    print ans
+                    for cat in ans:
+                        mystr = mystr + "\t" + cat
+                fileout.write("%s\n" % mystr.encode('utf-8','ignore'))
+                print ("%s\n" % mystr.encode('utf-8','ignore'))
+            except Exception as err:
+                sys.stderr.write("%s:%s\n" % (datetime.datetime.now(),err))
+
+if __name__ == '__main__':
+    main()
